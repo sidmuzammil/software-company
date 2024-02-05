7b742c55dc74c4a79b7206239fbccda8a1301cd1
sidmuzammil <sidhuxnm@gmail.com>
2024-02-05 20:15:10 +0530

    second commit

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -11,21 +11,21 @@ import {
 export const SocialMediaProfiles = [
   {
     title: "Youtube",
-    href: "https://www.youtube.com/@reactjsBD/",
+    href: "#",
     icon: BsYoutube,
   },
-  { title: "GitHub", href: "https://github.com/noorjsdivs/", icon: BsGithub },
+  { title: "GitHub", href: "https://github.com/sidmuzammil", icon: BsGithub },
   {
     title: "Facebook",
-    href: "https://www.facebook.com/Noorlalu143/",
+    href: "#",
     icon: BsFacebook,
   },
   {
     title: "Instagram",
-    href: "https://www.linkedin.com/in/noor-mohammad-ab2245193/",
+    href: "https://www.instagram.com/sid_muzamil_/",
     icon: BsLinkedin,
   },
-  { title: "Twitter", href: "https://twitter.com", icon: BsTwitter },
+  { title: "Twitter", href: "#", icon: BsTwitter },
 ];
 
 const SocialMedia = ({ className, invert = false }) => {
